780deb90e0aaddcd49a8333d6bb71cd3dc9c4582
FWMoor <30631816+FWMoor@users.noreply.github.com>
2019-06-04 10:27:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
- 
-void varstrings(int count, ...)   /* the ellipsis indicates variable arguments */
+#include <unistd.h>
+
+void ft_putchar(char c)
 {
+    write(1, &c, 1);
+}
+
+void varstrings(const char *format, ...)   /* the ellipsis indicates variable arguments */
+{
+    int i;
     va_list args;
-    va_start(args, count);
-    while (count--)
-        puts(va_arg(args, const char *));
+
+    i = 0;
+    va_start(args, format);
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'd')
+                printf("%d", va_arg(args, int));
+            i+=2;
+        }
+        else
+        {
+            printf("%c", format[i]);
+            i++;
+        }
+    }
     va_end(args);
 }
 
 int main(void)
 {
-    varstrings(2, "Hello", "there");
+    varstrings("%d %d", 2, 3);
 }
