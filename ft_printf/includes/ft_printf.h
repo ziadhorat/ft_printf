7101e83031bc1c9bd9c9aa2c14b6f572dc690f7b
Ziaul-Haque MAHOMED <zmahomed@c4r20s4.wethinkcode.co.za>
2019-07-12 10:16:48 +0200

    Its a start bois

--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -6,12 +6,15 @@
 /*   By: zmahomed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 11:05:17 by zmahomed          #+#    #+#             */
-/*   Updated: 2019/07/11 11:05:49 by zmahomed         ###   ########.fr       */
+/*   Updated: 2019/07/12 10:09:00 by zmahomed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include "../libft/libft.h"
+# include <stdarg.h>
+
+void ft_printf(const char *format, ...);
 
 #endif
